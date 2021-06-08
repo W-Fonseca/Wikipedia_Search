f85feee129c1c2d36bc9df9d76d16b695fe0ccf2
Wellington Fonseca <wj_fonseca@hotmail.com>
2021-06-08 10:22:53 -0300

    Wikipedia_Search
    Wikipedia_Search

---
+++ Wikipedia/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Wikipedia {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Pesquisa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Pesquisa
+            // 
+            this.Pesquisa.Location = new System.Drawing.Point(51, 12);
+            this.Pesquisa.Name = "Pesquisa";
+            this.Pesquisa.Size = new System.Drawing.Size(316, 20);
+            this.Pesquisa.TabIndex = 0;
+            this.Pesquisa.TextChanged += new System.EventHandler(this.Pesquisa_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(419, 308);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(417, 67);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Pesquisa);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Pesquisa Wikipédia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Pesquisa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Wikipedia/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+
+
+namespace Wikipedia {
+
+    public partial class Form1 : Form {
+          
+        public Form1() {
+            InitializeComponent();
+           
+        }
+        private void button1_Click(object sender, EventArgs e) {
+
+            textBox1.ResetText();
+            var webGet = new HtmlWeb();
+            if (webGet.Load("https://pt.wikipedia.org/w/index.php?search=" + Pesquisa.Text) is HtmlAgilityPack.HtmlDocument document) {
+                var nodes = document.DocumentNode.CssSelect("body").ToList().CssSelect("div.mw-parser-output").Single().CssSelect("p").Count();
+                for (int i = 0; i < nodes; i++) {
+                    textBox1.Text += document.DocumentNode.CssSelect("body").ToList().CssSelect("div.mw-parser-output").Single().CssSelect("p").ElementAt(i).InnerText;
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Visible = true;
+                    Form1.ActiveForm.Size = new System.Drawing.Size(445, 419);
+                }
+            }
+        }
+
+        private void Pesquisa_TextChanged(object sender, EventArgs e) {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            
+        }
+    }
+}
